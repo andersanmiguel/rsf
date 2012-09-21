0209cc1608e1fc1bc4e7b25840cf9bc15862ccff
Ander <andersanmiguel@gmail.com>
2012-09-21 17:40:56 +0200

    Removing especifics from local dev enviroment

--- application/conf/development_config.php
+++ application/conf/development_config.php
@@ -3,7 +3,7 @@ include 'config.php';
 // Display errors in development mode
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-$config['install_route'] = '/pruebas/framework';
+$config['install_route'] = '';
 
 $config['db_type'] = 'sqlite';
 $config['db'] = 'sqlite:'.BASE.'db/database.sqlite';
